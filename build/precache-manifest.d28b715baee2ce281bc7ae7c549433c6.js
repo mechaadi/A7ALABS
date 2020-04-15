096ca0954801b458fa84ecd01b08681fc15e52c2
mechaadi <as808780@gmail.com>
2020-04-16 02:27:45 +0530

    build

---
+++ build/precache-manifest.d28b715baee2ce281bc7ae7c549433c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e67dac0dd2f74c2c99d6928dc9181e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6fad5bd52d51ca92fea",
+    "url": "/static/css/main.9a2ad21e.chunk.css"
+  },
+  {
+    "revision": "63a577bcafea8ab48fd2",
+    "url": "/static/js/2.8d275427.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d275427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6fad5bd52d51ca92fea",
+    "url": "/static/js/main.03068ac8.chunk.js"
+  },
+  {
+    "revision": "442f44f2ba89ca1d8d11",
+    "url": "/static/js/runtime-main.ed55b2fa.js"
+  }
+]);
